41aa3a3a8a00a6727cbdddb4f73f64bc985d5c1b
justazrd <justazrd007@gmail.com>
2024-06-20 00:50:42 -0700

    Project Structure Modification

---
+++ frontend/lib/project/views/weather/recommendation_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../fields/home_field_detail_page.dart';
+class RecommendationCard extends StatelessWidget {
+  final Map<String, dynamic> location;
+
+  const RecommendationCard({super.key, required this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(location['image_url'] ?? ''),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(location['name'] ?? 'No Name',
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+                Text(location['location'] ?? 'No Location'),
+                Text('Rating: ${location['rating'] ?? 'No Rating'}'),
+                Text(
+                    'Difficulty: ${location['difficulty'] ?? 'No Difficulty'}'),
+                Text('Distance: ${location['distance'] ?? 'No Distance'} km'),
+                Text(
+                    'Estimated Time: ${location['estimated_time'] ?? 'No Estimated Time'}'),
+                const SizedBox(height: 8),
+                Align(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeFieldDetailPage(
+                              field: location), // 确保此处导入了HomeFieldDetailPage
+                        ),
+                      );
+                    },
+                    child: const Text('View Details'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ frontend/lib/project/views/weather/weather_home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:frontend/project/views/weather/recommendation_card.dart';
+import 'package:frontend/project/views/weather/weather_card.dart';
+
+class WeatherHome extends StatelessWidget {
+  final Map<String, dynamic> weatherData;
+  final Map<String, dynamic>? recommendationData;
+
+  WeatherHome({super.key, required this.weatherData, required this.recommendationData});
+
+  @override
+  Widget build(BuildContext context) {
+    bool badWeather =
+        recommendationData == null || recommendationData!['fields'].isEmpty;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          WeatherCard(weatherData: weatherData),
+          const SizedBox(height: 10),
+          if (badWeather)
+            Column(
+              children: [
+                const Text("The weather is not good for outdoor activities.",
+                    style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 10),
+                const Text("You can choose indoor activities.",
+                    style: TextStyle(fontSize: 16)),
+                const SizedBox(height: 10),
+                Image.asset('assets/image.png'), // 确保你的图片路径正确
+              ],
+            )
+          else
+            Column(
+              children: [
+                const Text("Today's Recommendation",
+                    style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 10),
+                Text(
+                  recommendationData?['advice'] ??
+                      'No recommendations available.',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                if (recommendationData != null)
+                  for (var location in recommendationData!['fields'])
+                    RecommendationCard(location: location),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
