dcd4efeb7d66423b766acd48f0ad3c8a43a3478a
Hongkun LIU <66795576+lhkun123@users.noreply.github.com>
2024-06-27 21:38:08 -0700

    Chat page Modification

--- frontend/lib/project/views/chat/chat_page.dart
+++ frontend/lib/project/views/chat/chat_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
-import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 
@@ -70,7 +69,7 @@ class _ChatPageState extends State<ChatPage> {
         }
       }
 
-      await OpenFilex.open(localPath);
+
     }
   }
 
